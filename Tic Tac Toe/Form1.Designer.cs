e5397d577c08c445001a2bdc8a712a08f198dd9a
Zach <Zach@ERIKDELL>
2024-07-09 23:41:31 -0500

    Add project files.

---
+++ Tic Tac Toe/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.Button7 = new System.Windows.Forms.Button();
+            this.Button4 = new System.Windows.Forms.Button();
+            this.Button5 = new System.Windows.Forms.Button();
+            this.Button3 = new System.Windows.Forms.Button();
+            this.Button8 = new System.Windows.Forms.Button();
+            this.Button9 = new System.Windows.Forms.Button();
+            this.Button6 = new System.Windows.Forms.Button();
+            this.TurnTextBox = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(445, 482);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // Button1
+            // 
+            this.Button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Button1.Location = new System.Drawing.Point(12, 0);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(139, 135);
+            this.Button1.TabIndex = 1;
+            this.Button1.UseVisualStyleBackColor = false;
+            this.Button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Button2
+            // 
+            this.Button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Button2.Location = new System.Drawing.Point(12, 140);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(139, 135);
+            this.Button2.TabIndex = 2;
+            this.Button2.UseVisualStyleBackColor = false;
+            this.Button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Button7
+            // 
+            this.Button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Button7.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Button7.Location = new System.Drawing.Point(302, 0);
+            this.Button7.Name = "Button7";
+            this.Button7.Size = new System.Drawing.Size(139, 135);
+            this.Button7.TabIndex = 3;
+            this.Button7.UseVisualStyleBackColor = false;
+            this.Button7.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // Button4
+            // 
+            this.Button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Button4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Button4.Location = new System.Drawing.Point(157, -1);
+            this.Button4.Name = "Button4";
+            this.Button4.Size = new System.Drawing.Size(139, 135);
+            this.Button4.TabIndex = 4;
+            this.Button4.UseVisualStyleBackColor = false;
+            this.Button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Button5
+            // 
+            this.Button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Button5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Button5.Location = new System.Drawing.Point(157, 140);
+            this.Button5.Name = "Button5";
+            this.Button5.Size = new System.Drawing.Size(139, 135);
+            this.Button5.TabIndex = 5;
+            this.Button5.UseVisualStyleBackColor = false;
+            this.Button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Button3
+            // 
+            this.Button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Button3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Button3.Location = new System.Drawing.Point(12, 281);
+            this.Button3.Name = "Button3";
+            this.Button3.Size = new System.Drawing.Size(139, 135);
+            this.Button3.TabIndex = 6;
+            this.Button3.UseVisualStyleBackColor = false;
+            this.Button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Button8
+            // 
+            this.Button8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Button8.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Button8.Location = new System.Drawing.Point(302, 140);
+            this.Button8.Name = "Button8";
+            this.Button8.Size = new System.Drawing.Size(139, 135);
+            this.Button8.TabIndex = 7;
+            this.Button8.UseVisualStyleBackColor = false;
+            this.Button8.Click += new System.EventHandler(this.Button8_Click);
+            // 
+            // Button9
+            // 
+            this.Button9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Button9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Button9.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Button9.Location = new System.Drawing.Point(302, 281);
+            this.Button9.Name = "Button9";
+            this.Button9.Size = new System.Drawing.Size(139, 135);
+            this.Button9.TabIndex = 8;
+            this.Button9.UseVisualStyleBackColor = false;
+            this.Button9.Click += new System.EventHandler(this.Button9_Click);
+            // 
+            // Button6
+            // 
+            this.Button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Button6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Button6.Location = new System.Drawing.Point(157, 281);
+            this.Button6.Name = "Button6";
+            this.Button6.Size = new System.Drawing.Size(139, 135);
+            this.Button6.TabIndex = 9;
+            this.Button6.UseVisualStyleBackColor = false;
+            this.Button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // TurnTextBox
+            // 
+            this.TurnTextBox.AutoSize = true;
+            this.TurnTextBox.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.TurnTextBox.Location = new System.Drawing.Point(26, 437);
+            this.TurnTextBox.Name = "TurnTextBox";
+            this.TurnTextBox.Size = new System.Drawing.Size(90, 16);
+            this.TurnTextBox.TabIndex = 10;
+            this.TurnTextBox.Text = "Player 1\'s turn";
+            this.TurnTextBox.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(315, 425);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 48);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Player 1\'s Score :0\r\nPlayer 2\'s Score: 0\r\nTies : 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 482);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TurnTextBox);
+            this.Controls.Add(this.Button6);
+            this.Controls.Add(this.Button9);
+            this.Controls.Add(this.Button8);
+            this.Controls.Add(this.Button3);
+            this.Controls.Add(this.Button5);
+            this.Controls.Add(this.Button4);
+            this.Controls.Add(this.Button7);
+            this.Controls.Add(this.Button2);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.Button Button2;
+        private System.Windows.Forms.Button Button7;
+        private System.Windows.Forms.Button Button4;
+        private System.Windows.Forms.Button Button5;
+        private System.Windows.Forms.Button Button3;
+        private System.Windows.Forms.Button Button8;
+        private System.Windows.Forms.Button Button9;
+        private System.Windows.Forms.Button Button6;
+        private System.Windows.Forms.Label TurnTextBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Tic Tac Toe/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        int turncount = 0; //shows how many turns have passed
+        private bool isplayerXturn = true; //X's turn
+        private bool isplayerYturn = false;// Y's turn. This isn't necessary except to show who's turn it is
+        //(there is a text box in the app that show's whos turn it is, so the boolean is neccesary)
+        public Image xImage = Properties.Resources.font_illustration_letter_x_EM72Y5; //gets the X image from resources that I downloaded
+        public Image oImage = Properties.Resources.font_illustration_letter_o_EM737K; //same as above, but for O
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            AttachClickHandlers();
+
+        }
+        private void AttachClickHandlers()
+        {
+            // Attach Button9_Click to all buttons' Click event
+            this.Button1.Click += Button9_Click;
+            this.Button2.Click += Button9_Click;
+            this.Button3.Click += Button9_Click;
+            this.Button4.Click += Button9_Click;
+            this.Button5.Click += Button9_Click;
+            this.Button6.Click += Button9_Click;
+            this.Button7.Click += Button9_Click;
+            this.Button8.Click += Button9_Click;
+            this.Button9.Click += Button9_Click;
+
+            //Initalizing the text box to show who's turn it is
+            UpdateTurnTextBox();
+
+        }
+
+
+        private void UpdateTurnTextBox()
+        {
+            TurnTextBox.Text = isplayerXturn ? "Player X's turn" : "Player O's turn";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button9_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            if (button == null)
+            {
+                MessageBox.Show("Button is null");
+                return;
+            }
+            if (button.BackgroundImage == null)
+
+            {
+                button.BackgroundImage = isplayerXturn ? xImage : oImage;
+                // turn booleans stated below
+                isplayerXturn = !isplayerXturn;
+                isplayerYturn = !isplayerYturn;
+                //Text box turn code
+                UpdateTurnTextBox();
+                //turncount with the ++ is the turn counter used by the game. turncount was declared an int variable earlier
+                turncount++;
+                //this is debug code below, still have not integrated it fully.
+                Console.WriteLine($"Button clicked. isplayerXTurn is now: {isplayerXturn}, turnCount is now: {turncount}");
+            }
+
+        }
+    }
+}
